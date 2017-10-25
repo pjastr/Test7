ac51385d74824b1a22b621df2805886715d1a4bc
Piotrek <piotr@niepodam.pl>
2017-10-25 17:30:08 +0200

    first com

---
+++ ConsoleApplication27/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication27
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region dzielenie
+            int liczba1;
+            //liczba1 = 30 / 0;
+            int liczba2 = 0, liczba3 = 30;
+            try
+            {
+                liczba1 = liczba3 / liczba2;
+            }
+            catch 
+            {
+                Console.WriteLine("wyjątek");
+            }
+            
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            #endregion
+            Console.ReadLine();
+        }
+             
+    }
+}
